89d682c069891e91eb3e444a54386556dc364959
Philip Hansen <me@philiphansen.dk>
2014-06-10 03:03:56 +0200

    Base mod
    The mod now logs an amazing message when loaded.
    
    Such use.
    Many applications.
    Wow.

---
+++ src/main/java/net/creeperhost/chtweaks/reference/ModInfo.java
@@ -0,0 +1,7 @@
+package net.creeperhost.chtweaks.reference;
+
+public class ModInfo {
+	public static final String ID = "chtweaks";
+	public static final String NAME = "CH Tweaks";
+	public static final String VERSION = "${version}";
+}
